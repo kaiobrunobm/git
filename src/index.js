b619635ba8bf3a9870cf515a358e47471012b721
Kaio Bruno Barbosa Magalhães <bmkaiobruno@gmail.com>
2026-01-08 21:54:18 -0300

    feat(new console): add a new log into the index file

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import sum from "./utils/sum.js";
 
 console.log('This is the branch responsable to code the feature of merge branch');
 console.log(sum(1, 3));
+
+console.log('this is a merge test')
